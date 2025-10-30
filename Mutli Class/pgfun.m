7712173ba6f1e941b61a5de145db8db1235c3e18
Dongpo Xu <55781800+xudp100@users.noreply.github.com>
2025-10-30 09:07:07 +0800

    Delete Mutli Class directory

--- Mutli Class/pgfun.m
+++
@@ -1,8 +0,0 @@
-function g = pgfun(x,ind, A, b, K)
-  bind = b(ind);
-  Aind = A(ind, :);
-  Y_hot = full(ind2vec(bind', K))';
-  Y_pred = softmax((Aind * x)')';
-  diff = Y_pred - Y_hot;
-  g = Aind' * diff/length(ind);
-end
